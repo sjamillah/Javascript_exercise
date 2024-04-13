44528011c69733f629ee8d9b044f5be5123be255
sjamillah <j.ssozi@alustudent.com>
2024-04-13 09:56:40 +0300

    tests the count of vowels solution

---
+++ Tests/test_Solutions/test_vowelcount.js
@@ -0,0 +1,11 @@
+'''tests the vowel count script'''
+
+import getCount from '../Solutions/vowelcount';
+
+const {assert} = require("chai");
+
+describe("Vowels Count Tests",function(){
+  it("should return 5 for 'abracadabra'",function(){
+    assert.strictEqual(getCount("abracadabra"), 5) ;
+  });
+});
