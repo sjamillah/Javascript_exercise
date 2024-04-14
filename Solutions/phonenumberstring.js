385abc800a78355635d3a815eec801b1e03d714f
sjamillah <j.ssozi@alustudent.com>
2024-04-14 20:06:56 +0300

    phone number creation using string literals

--- Solutions/phonenumberstring.js
+++ Solutions/phonenumberstring.js
@@ -1,6 +1,6 @@
 '''Gets numbers from 0-9 and makes a phone number string'''
 function createPhoneNumber(numbers){
-  let str = `(${numbers.slice(0, 3).join('')}) ${numbers.slice(3, 6).join('')}-${numbers.slice(6).join('')}`;
+  let str = `(${numbers.slice(0, 3).join('')}) ${numbers.slice(3, 6).join('')}-${numbers.slice(6).join('')}`; //slices the first three numbers, the next three with a space between and last four with parantheses between
   return str;
 }
 
