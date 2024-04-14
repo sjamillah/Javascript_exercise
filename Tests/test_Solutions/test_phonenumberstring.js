686efdfa17e6b8aad6641de740352c7c3f7807b3
sjamillah <j.ssozi@alustudent.com>
2024-04-14 20:04:54 +0300

    tests phonenumberstring

---
+++ Tests/test_Solutions/test_phonenumberstring.js
@@ -0,0 +1,16 @@
+'''tests the phone number function'''
+
+import createPhoneNumber from '../Solutions/phonenumberstring';
+
+
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Create Phone Number", () => {
+  it("Fixed tests", () => {
+    assert.strictEqual(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]), "(123) 456-7890");
+    assert.strictEqual(createPhoneNumber([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]), "(111) 111-1111");
+    assert.strictEqual(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]), "(123) 456-7890");
+  });
+});
